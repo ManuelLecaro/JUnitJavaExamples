cc617813de8bae4bbcd0cf68a7f56bd0d2059a28
Daniela Montenegro <dcmonten@espol.edu.ec>
2018-08-16 13:24:52 -0500

    comentario en assert not null

--- JUnitPruebas/src/junitpruebas/AssertNull.java
+++ JUnitPruebas/src/junitpruebas/AssertNull.java
@@ -12,6 +12,8 @@ public class AssertNull {
 	public AssertNull() {
 	}
 	
+	//mapa con valores fijos
+	
     public String getMapValue(final String key){
         Map<String, String> map = new HashMap<>();
         map.put("key1", "value 1");
@@ -19,7 +21,8 @@ public class AssertNull {
         map.put("key3", "value 3");
         return map.get(key);
     }
-     
+     //este test prueba si la funcion retorna un objeto con la clave
+    //si se pone "key9", el test fallara, solo con key1, key2 o key3 funcionara
     @Test
     public void test(){
     	
